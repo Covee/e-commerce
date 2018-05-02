f554a79e6854eb1477af0987ce8668a177bd9a62
Covee <uniqtop91@gmail.com>
2018-05-03 08:22:21 +0900

    first commit yeah

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cobee/Desktop/django_files/ecommerce_django/v_ecommerce/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
