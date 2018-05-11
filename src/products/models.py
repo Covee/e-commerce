958fd7f7b251aad2ef1b71245e12d80edda2f111
Covee <uniqtop91@gmail.com>
2018-05-11 18:01:53 +0900

    slugfield 설정(db에 초기 products의 slug를 설정해주고 migrate해야 에러안남), signal 설정

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,9 @@ import random
 import os
 
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 
 def get_filename_ext(filepath):
@@ -41,6 +44,7 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
 	title = models.CharField(max_length=120)
+	slug = models.SlugField(blank=True, unique=True)
 	description = models.TextField()
 	price = models.DecimalField(decimal_places=2, max_digits=20, default=9900)
 	image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
@@ -55,3 +59,13 @@ class Product(models.Model):
 	# for Python 2
 	def __unicode__(self):
 		return self.title
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+
