69c4b507bab959ec213e79b5e85c0f12450b6b18
Covee <uniqtop91@gmail.com>
2018-05-14 10:01:51 +0900

    tag search 추가

--- src/products/models.py
+++ src/products/models.py
@@ -37,7 +37,8 @@ class ProductQuerySet(models.query.QuerySet):
 		lookups = (
 			Q(title__icontains=query) |
 			Q(description__icontains=query) |
-			Q(price__icontains=query)
+			Q(price__icontains=query) |
+			Q(tag__title__icontains=query)
 			)
 		return self.filter(lookups).distinct()
 
