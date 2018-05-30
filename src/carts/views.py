071065a7f66f5820581aa7b3c70ef0fd3fa67e83
Covee <uniqtop91@gmail.com>
2018-05-30 21:28:34 +0900

    session

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def cart_home(request):
+	request.session['cart_id'] = 12
+	request.session['user'] = request.user.username
 	return render(request, "carts/home.html", {})
+
